a0790b592dec1b23270c80878d649b6e7a1d9877
cantwork <t.co@qq.com>
2022-03-19 22:13:30 +0800

    Update tieba_h5_ad.js

--- tieba_h5_ad.js
+++ tieba_h5_ad.js
@@ -6,8 +6,11 @@ const notifiTitle = "去广告脚本错误";
 
 let body = JSON.parse($response.body);
 
+console.log('贴吧h5-帖子详情');
+console.log(body);
+
 if(url.indexOf("tieba.baidu.com/mg/p/getPbData") != -1 && method == getMethod){
-  console.log('贴吧h5-帖子详情页');
+  console.log('贴吧h5-帖子详情');
   if (body.res && body.res.data) {
     body.res.data.asp_ad_list=null;
     body.res.data.banner_list=null;
@@ -16,6 +19,16 @@ if(url.indexOf("tieba.baidu.com/mg/p/getPbData") != -1 && method == getMethod){
   }else{
     $notification.post(notifiTitle, "贴吧-getPbData", "广告为undefined");
   }
+} else if (url.indexOf("tieba.baidu.com/mo/q/recommendpb/recomSiteTids") != -1 && method == postMethod) {
+  console.log('贴吧h5-帖子底部推荐');
+  if (body.res && body.res.data) {
+    body.res.data.hot_list=[];
+    body.res.data.site_list=[];
+    console.log('成功');
+  }else{
+    $notification.post(notifiTitle, "贴吧-recomSiteTids", "广告为undefined");
+  }
+  
 }
 
 body = JSON.stringify(body);
