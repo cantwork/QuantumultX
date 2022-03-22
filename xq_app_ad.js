19333795dd0822ac3f538b4518b6e4cf92675a2c
cantwork <t.co@qq.com>
2022-03-22 20:25:16 +0800

    Create xq_app_ad.js

---
+++ xq_app_ad.js
@@ -0,0 +1,23 @@
+const url = $request.url;
+const method = $request.method;
+const getMethod = "GET";
+const postMethod = "POST";
+const notifiTitle = "去广告脚本错误";
+
+let body = JSON.parse($response.body);
+
+if(url.indexOf("getPbData") != -1 && method == getMethod){
+  console.log('雪球/statuses/show.json');
+  if (body && body.title_ad_pic) {
+    body.title_ad_pic=null;
+    console.log('成功');
+  }else{
+    $notification.post(notifiTitle, "雪球show.json", "广告为undefined");
+  }
+} 
+
+body = JSON.stringify(body);
+
+$done({
+    body
+});
